57b094651e2ca430b80813bdadf6409995b15ff9
hamzaeldemery <hkhehmad@gmail.com>
2022-04-11 03:33:49 +0800

    Feat: add state variables, render screen based on values

--- lib/query.dart
+++ lib/query.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Query extends StatefulWidget {
   final clubs; 
-  const Query(this.clubs);
+  Query(this.clubs);
 
   @override
   QueryState createState() => QueryState(clubs);
@@ -14,7 +14,9 @@ class QueryState extends State<Query> {
   
   final myState = TextEditingController();
   final clubs; 
-
+  bool isAnswer = false;
+  String answer = "";
+  
   @override
   void dispose() {
     myState.dispose();
@@ -28,18 +30,68 @@ class QueryState extends State<Query> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children:  <Widget>[
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 16),
-          child: TextField(
-            controller: myState,
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter a number',
+        Column(
+          children:[
+            if(!isAnswer)
+            ...[
+              Text(
+              'Choose a number?',
+              style: Theme.of(context).textTheme.headline4,
             ),
-          ),
-        ),
-      ]
+              Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 16),
+                child: TextField(
+                  controller: myState,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter a number',
+                  ),
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 150, vertical: 25),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: Colors.red),
+                    onPressed: () => { 
+                      
+                      if((int.parse(myState.text)) > clubs.length || (int.parse(myState.text)) <  0 ){
+                        setState((){
+                          isAnswer = true;
+                          answer = "There is only ${clubs.length} clubs";
+                        }),
+                      }
+                      else{
+                        setState((){
+                          isAnswer = true;
+                          answer = clubs[(int.parse(myState.text))-1];
+                        }),
+                      }
+                    },
+                    child: Text("Enter"),
+                  ),
+              ),
+            ]else 
+            ...[
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 150, vertical: 25),
+                  child: Text(answer)
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.red),
+                onPressed: () => { 
+                      setState((){
+                          isAnswer = false;
+                      }),
+                },
+                child: Text("Return"),
+              ),
+            ]
+          ],
+        )
+      ],
+          
+        
     );
   }
 }
